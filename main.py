55f54b775d69ed63666357bc8cb12f65304716d6
napolizer <napolizer@op.pl>
2022-04-17 20:51:54 +0200

    Poprawki w czytelności i działaniu kodu. (Nie dotykałem poprawności działania)

--- main.py
+++ main.py
@@ -1,54 +1,49 @@
 # Interpolacja Lagrange'a dla węzłów Czebyszewa
 
 from math import cos, pi
+from collections import namedtuple
 import numpy as np
 
+Point = namedtuple('Point', 'x y')
 
 # obliczanie wartości funkcji na podstawie wybranej funkcji i x
 def calculate_y(func_choice, x):
-    match func_choice:
-        case '1':
-            return 2 * x + 2
+    if func_choice == '1':
+        return 2 * x + 2
 
 
 # obliczanie x i y dla wybranej funkcji na przedziale (a, b) z zadanym krokiem
 def calculate_function(a, b, func_choice, step=0.01):
-    x_list = []
-    y_list = []
+    points = []
     for i in np.arange(a, b, step):
-        x_list.append(i)
-        y_list.append(calculate_y(func_choice, i))
-    return x_list, y_list
+        points.append(Point(i, calculate_y(func_choice, i)))
+    return points
 
 
 # obliczanie wezlow Czebyszewa
 def calculate_nodes(number_of_nodes, a, b, func_choice):
-    nodes_x = []
-    nodes_y = []
+    nodes = []
     for i in range(0, number_of_nodes):
         x = cos(pi * (2 * i + 1) / (2 * number_of_nodes + 1))
         x = (((b - a) * x) + (a + b)) / 2
 
-        nodes_x.append(x)
-        nodes_y.append(calculate_y(func_choice, x))
-    return nodes_x, nodes_y
+        nodes.append(Point(x, calculate_y(func_choice, x)))
+    return nodes
 
 
 # interpolacja Lagrange'a
-def interpolation(number_of_nodes, nodes_x, nodes_y, a, b, step=0.01):
-    interpolated_x = []
-    interpolated_y = []
+def interpolation(number_of_nodes, nodes, a, b, step=0.01):
+    interpolated_points = []
     for i in np.arange(a, b, step):
         y = 0
         for j in range(0, number_of_nodes):
-            helper = nodes_y[j]
+            helper = nodes[j].y
             for k in range(0, number_of_nodes):
                 if j != k:
-                    helper *= ((i - nodes_x[k]) / (nodes_x[j] - nodes_x[k]))
+                    helper *= ((i - nodes[k].x) / (nodes[j].x - nodes[k].x))
             y += helper
-        interpolated_x.append(i)
-        interpolated_y.append(y)
-    return interpolated_x, interpolated_y
+        interpolated_points.append(Point(i, y))
+    return interpolated_points
 
 
 def main():
@@ -72,9 +67,11 @@ def main():
         print("Podaj ilość węzłów interpolacyjnych")
         number_of_nodes = input("\t\t>>> ")
 
-    original_func_x, original_func_y = calculate_function(float(a), float(b), func_choice)
-    nodes_x, nodes_y = calculate_nodes(int(number_of_nodes), float(a), float(b), func_choice)
-    inter_x, inter_y = interpolation(int(number_of_nodes), nodes_x, nodes_y, float(a), float(b))
+    #original_func_x, original_func_y = calculate_function(float(a), float(b), func_choice)
+    nodes = calculate_nodes(int(number_of_nodes), float(a), float(b), func_choice)
+    interpolated_points = interpolation(int(number_of_nodes), nodes, float(a), float(b))
+    for point in interpolated_points:
+        print(f"x: {point.x}, y: {point.y}")
 
 
 if __name__ == "__main__":
